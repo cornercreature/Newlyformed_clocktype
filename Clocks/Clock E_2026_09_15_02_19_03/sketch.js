7e015c45405fd19c7021d06784468a261bafe7cc
Nicole Sun <nsun0511@gmail.com>
2026-09-15 14:47:46 -0400

    clocksketch Edebug1

--- Clocks/Clock E_2026_09_15_02_19_03/sketch.js
+++ Clocks/Clock E_2026_09_15_02_19_03/sketch.js
@@ -49,7 +49,7 @@ function draw() {
   // Calculate angle for each hand
   secondAngle = draggingHand === 'second' ? getMouseAngle() : paused ? pausedSecondAngle : (map(second(), 0, 60, 0, 360) + secondOffset);
   minuteAngle = draggingHand === 'minute' ? getMouseAngle() : paused ? pausedMinuteAngle : (map(minute(), 0, 60, 0, 360) + minuteOffset);
-  trailAngle = draggingHand === 'trail' ? getMouseAngle() : paused ? pausedTrailAngle : (-(minuteAngle - secondAngle )/2 + trailOffset);
+  trailAngle = draggingHand === 'trail' ? getMouseAngle() : paused ? pausedTrailAngle : (minuteAngle - secondAngle )/2 + trailOffset;
  
   //arc 1
   noFill();
